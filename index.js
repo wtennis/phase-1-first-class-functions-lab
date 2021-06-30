23388bd8aad83d1239707df99cf815fa6a2d5cb5
Whiting <whitinghtennis@gmail.com>
2021-06-29 20:52:01 -0700

    commit tree 60f4267d8bca83ccd4ab55778a2394f1b4f2acd9

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const drivers = ['Mario', 'Yoshi', 'Peach', 'Luigi'];
+
+function returnFirstTwoDrivers(drivers) {
+return (drivers.slice(0,2));
+}
+
+function returnLastTwoDrivers(drivers) {
+    return(drivers.slice(-2))
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+return function fareMultiplier(fare) {
+    return fare*multiplier
+}
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, func) {
+return func(drivers)
+}
+
